9207579170c974c3b025380e22789629b5869cd5
Gustavo Silva Vieira <gstvsvieira@gmail.com>
2024-03-25 15:18:56 -0300

    random fix

--- src/js/random.js
+++ src/js/random.js
@@ -13,7 +13,9 @@ const listasAnimes = {
     drama: [],
     ecchi: [],
     terror: [],
-    suspense: []
+    suspense: [],
+    adventure: [],
+    romance: []
 };
 
 const elementosItens = document.querySelectorAll(".itens");
@@ -26,7 +28,7 @@ elementosItens.forEach((item, i) => {
         const imageSrc = imageItem.src;
         let nomeArquivo = imageSrc.replace(/\.jpg$/, ''); // Remove a extensão .jpg
         nomeArquivo = nomeArquivo.substring(nomeArquivo.lastIndexOf('/') + 1);
-
+        console.log(nomeArquivo);
         if (item.classList.contains('action')) {
             listasAnimes.action.push(nomeArquivo);
         } else if (item.classList.contains('fantasy')) {
@@ -41,6 +43,10 @@ elementosItens.forEach((item, i) => {
             listasAnimes.terror.push(nomeArquivo);
         } else if (item.classList.contains('suspense')) {
             listasAnimes.suspense.push(nomeArquivo);
+        } else if (item.classList.contains('adventure')) {
+            listasAnimes.adventure.push(nomeArquivo);
+        } else if (item.classList.contains('romance')) {
+            listasAnimes.romance.push(nomeArquivo);
         }
 
         listasAnimes.all.push(nomeArquivo);
@@ -112,7 +118,6 @@ botoes.forEach((botao,i) => {
             })
 
         }else if(botao.classList.contains("terror")){
-            lista = [" "];
             lista = listasAnimes.terror; 
 
             escolhido = formatarNome(lista,resultado);
@@ -123,7 +128,6 @@ botoes.forEach((botao,i) => {
             })
 
         }else if(botao.classList.contains("suspense")){
-            lista = [" "];
             lista = listasAnimes.suspense; 
 
             escolhido = formatarNome(lista,resultado);
@@ -133,6 +137,24 @@ botoes.forEach((botao,i) => {
                 window.location.href = "./../../src/html/animes/" + lista[escolhido] + ".html";
             })
 
+        }else if(botao.classList.contains("romance")){
+            lista = listasAnimes.romance; 
+        
+            escolhido = formatarNome(lista,resultado);
+            
+            resultadoImage.src = "./../../../assets/images/animes/" + lista[escolhido] + ".jpg"
+            resultado.addEventListener("click", () => {
+                window.location.href = "./../../src/html/animes/" + lista[escolhido] + ".html";
+            })
+        }else if(botao.classList.contains("adventure")){
+            lista = listasAnimes.adventure; 
+        
+            escolhido = formatarNome(lista,resultado);
+            
+            resultadoImage.src = "./../../../assets/images/animes/" + lista[escolhido] + ".jpg"
+            resultado.addEventListener("click", () => {
+                window.location.href = "./../../src/html/animes/" + lista[escolhido] + ".html";
+            })
         }
 
     });
